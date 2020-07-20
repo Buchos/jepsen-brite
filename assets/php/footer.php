ee3eb151b11b3fcabfe1482219be7d5db0fc8f87
artiart <art.kouznetsov@gmail.com>
2020-07-20 15:02:34 +0200

    initializing arti

---
+++ assets/php/footer.php
@@ -0,0 +1,6 @@
+<footer>
+    &copy; <?php echo date('F Y'); ?> JepsenBrite - Arthur, Greg, Artjom
+</footer>
+
+</body>
+</html>
---
+++ assets/php/header.php
@@ -0,0 +1,16 @@
+<?php
+if (!isset($page_title)) {
+    $page_title = 'Page Title not set!';
+}
+?>
+
+<!doctype html>
+
+<html lang="en">
+<head>
+    <title>ABI <?php echo $page_title; ?></title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="<?php echo 'stylesheets/jb.css'; ?>">
+</head>
+
+<body>

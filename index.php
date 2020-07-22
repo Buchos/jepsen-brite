d2dd76650f25cb0595ecf8a4fd8c0d4cec9e44db
artiart <art.kouznetsov@gmail.com>
2020-07-22 12:20:52 +0200

    arti

--- index.php
+++ index.php
@@ -3,16 +3,20 @@
 <?php require('assets/php/header.php') ?>
 
 <?php
+$today = date('Y-m-j');
 $bdd = new PDO('mysql:host=localhost;dbname=jepsen-brite', 'root', 'sqlPASS3', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
-$response = $bdd->query('SELECT * FROM events');
+$response = $bdd->query('SELECT * FROM `events` ORDER BY `date`');
 while ($data = $response->fetch()) {
-    echo '<article class="event-entry">
+    // display event ONLY if date > today
+    if ($data['date']>$today) {
+        echo '<article class="event-entry">
             <h3 class="event-title">' . $data['title'] . '</h3>
             <p class="event-date">' . $data['date'] .'</p>
             <p class="event-organizer"> Organized by ' . $data['organizer'] . '</p>
             <img src="" alt="Here will be the image ">' . $data['image'] . '
             <p class="event-description">' . $data['description'] . '</p>
-    </article>';
+        </article>';
+    }
 }
 ?>
 

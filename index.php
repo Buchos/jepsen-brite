a6fd7ea45b5d5c0b1d8d7d38f824484695433e87
artiart <art.kouznetsov@gmail.com>
2020-07-22 17:05:45 +0200

    arti wip

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php require_once('assets/php/initialize.php') ?>
-<?php $page_title = 'JepsenBrite Homepage' ?>
+<?php $page_title = 'Homepage' ?>
 <?php require('assets/php/header.php') ?>
 <?php require('assets/php/nav.php')?>
 
@@ -9,6 +9,9 @@ var_dump($today);
 //$bdd = new PDO('mysql:host=localhost;dbname=jepsen-brite', 'root', 'sqlPASS3', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
 $bdd = new PDO('mysql:host=us-cdbr-east-02.cleardb.com;dbname=heroku_cc256803d465131', 'bd60e8ee909b42', '2db04edd', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
 $response = $bdd->query('SELECT * FROM `events` ORDER BY `date`');
+// Show Events of certain category
+//$response = $bdd->prepare('SELECT * FROM `events` WHERE `category` = ? ORDER BY `date`');
+//$response->execute(array($_GET['category']));
 while ($data = $response->fetch()) {
     // display event ONLY if date > today
     if ($data['date']>$today) {
--- pages/category.php
+++ pages/category.php
@@ -1,1 +1,35 @@
-<?php
+<?php require_once('../assets/php/initialize.php') ?>
+<?php $page_title = 'Categories' ?>
+<?php require('../assets/php/header.php') ?>
+<?php require('../assets/php/nav.php')?>
+
+<ul>
+    <?php
+    $bdd = new PDO('mysql:host=us-cdbr-east-02.cleardb.com;dbname=heroku_cc256803d465131', 'bd60e8ee909b42', '2db04edd', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+    $categories = $bdd->query('SELECT * FROM `categories`');
+    while ($cats = $categories->fetch()) {
+        echo '<li>' . $cats['Name'] .'</li>';
+    }
+    ?>
+</ul>
+<!--<section class="events-container">-->
+<!--    --><?php
+//    // Show Events of certain category
+//    $response = $bdd->prepare('SELECT * FROM `events` WHERE `category` = ? ORDER BY `date`');
+//    $response->execute(array($_GET['category']));
+//    while ($data = $response->fetch()) {
+//        // display event ONLY if date > today
+//        if ($data['date']>$today) {
+//            echo '<article class="event-entry">
+//        <h3 class="event-title">' . $data['title'] . '</h3>
+//        <p class="event-date">' . $data['date'] .'</p>
+//        <p class="event-author"> Organized by ' . $data['author'] . '</p>
+//        <img src="" alt="Here will be the image ">' . $data['image'] . '
+//        <p class="event-description">' . $data['description'] . '</p>
+//    </article>';
+//        }
+//    }
+//    ?>
+<!--</section>-->
+
+<?php require('../assets/php/footer.php');

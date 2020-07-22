3676e9fd1f91340d31e7b28ac1faf5e0cf32325e
becode <dicaro.gregory@gmail.com>
2020-07-22 11:51:54 +0200

    mise ajour

---
+++ assets/php/nav.php
@@ -0,0 +1,25 @@
+<nav>
+    <ul>
+        <li><a href="header2.php">Home</a></li>
+        <li><a href="categorie.php">categorie</a></li>
+        <li><a href="pastEvent.php">Past-Event</a></li>
+
+        <div class="formulaire">
+        <form method="post" action="login.php">
+        <p>
+                
+            <button>LOGIN</button>
+            <button>SUBSCRIBE</button>
+                
+        </p>
+        
+                    
+        </form>
+       
+        
+    </ul>
+
+   
+
+</div>
+</nav>

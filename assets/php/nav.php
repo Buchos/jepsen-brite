19b1f03b3973724f4d22fbf0960837b4d555d488
artiart <art.kouznetsov@gmail.com>
2020-07-28 12:10:48 +0200

    category events number counter

--- assets/php/nav.php
+++ assets/php/nav.php
@@ -1,11 +1,13 @@
 <?php
+// GET NUMBER OF EVENTS FOR EACH CATEGORY
 $categories = $bdd->query('SELECT * FROM `categories`');
 while ($cats = $categories->fetch()) {
     $catname = $cats['name'];
-    $catcount = $bdd->prepare('SELECT COUNT(*) AS catcount FROM `events` WHERE `category`= ? AND `date` >= ?');
+    $catcount = $bdd->prepare('SELECT COUNT(*) AS catcount FROM `events` WHERE `category`= ? AND `date` >= ? AND `deleted` = 0');
     $catcount->execute(array($catname, $today));
     $numbofcat = $catcount->fetch();
-    ${'numbof' . $catname} = $numbofcat;
+    // CREATE A VARIABLE FOR EACH CATEGORY
+    ${'numbof' . $catname} = $numbofcat[0];
 }
 ?>
         <nav>
@@ -13,20 +15,19 @@ while ($cats = $categories->fetch()) {
             <div class="dropdown">
                 <button class="dropbtn">Categories</button>
                 <div class="dropdown-content">
-                    <a href="<?php echo $stupidroot;?>pages/categories.php?category=birthday">birthday</a>
-                    <a href="<?php echo $stupidroot;?>pages/categories.php?category=cinema">cinema</a>
-                    <a href="<?php echo $stupidroot;?>pages/categories.php?category=concert">concert</a>
-                    <a href="<?php echo $stupidroot;?>pages/categories.php?category=conference">conference</a>
-                    <a href="<?php echo $stupidroot;?>pages/categories.php?category=culture">culture</a>
-                    <a href="<?php echo $stupidroot;?>pages/categories.php?category=dance">dance</a>
-                    <a href="<?php echo $stupidroot;?>pages/categories.php?category=exhibition">exhibition</a>
-                    <a href="<?php echo $stupidroot;?>pages/categories.php?category=meetup">meetup</a>
-                    <a href="<?php echo $stupidroot;?>pages/categories.php?category=sport">sport</a>
-                    <a href="<?php echo $stupidroot;?>pages/categories.php?category=other">other</a>
+                    <a href="<?php echo $stupidroot;?>pages/categories.php?category=birthday">birthday - (<?php echo $numbofbirthday; ?>)</a>
+                    <a href="<?php echo $stupidroot;?>pages/categories.php?category=cinema">cinema - (<?php echo $numbofcinema; ?>)</a>
+                    <a href="<?php echo $stupidroot;?>pages/categories.php?category=concert">concert - (<?php echo $numbofconcert; ?>)</a>
+                    <a href="<?php echo $stupidroot;?>pages/categories.php?category=conference">conference - (<?php echo $numbofconference; ?>)</a>
+                    <a href="<?php echo $stupidroot;?>pages/categories.php?category=culture">culture - (<?php echo $numbofculture; ?>)</a>
+                    <a href="<?php echo $stupidroot;?>pages/categories.php?category=dance">dance - (<?php echo $numbofdance; ?>)</a>
+                    <a href="<?php echo $stupidroot;?>pages/categories.php?category=exhibition">exhibition - (<?php echo $numbofexhibition; ?>)</a>
+                    <a href="<?php echo $stupidroot;?>pages/categories.php?category=meetup">meetup - (<?php echo $numbofmeetup; ?>)</a>
+                    <a href="<?php echo $stupidroot;?>pages/categories.php?category=sport">sport - (<?php echo $numbofsport; ?>)</a>
+                    <a href="<?php echo $stupidroot;?>pages/categories.php?category=other">other - (<?php echo $numbofother; ?>)</a>
                 </div>
             </div>
             <a href="http://becode.local/jepsen-brite/pages/past.php">Past Events</a>
             <a href="http://becode.local/jepsen-brite/pages/login.php">LOGIN</a>
         </nav>
         <section class="main-grid">
-            <?php var_dump($numbofmeetup);?>

4f70e5902c783193b2135d3acb71eae00ccc177f
Buchos <pluyart@gmail.com>
2020-07-28 14:41:02 +0200

    update delete

--- pages/deleteuser.php
+++ pages/deleteuser.php
@@ -4,7 +4,7 @@ session_start();
 $bdd = new PDO('mysql:host=us-cdbr-east-02.cleardb.com;dbname=heroku_cc256803d465131', 'bd60e8ee909b42', '2db04edd', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
 
 $deleteUser = $db->prepare("UPDATE `users` SET `deleted`= ('1') WHERE id = ?");
-        $deleteEvent->execute(array($_SESSION['id']));
+        $deleteUser->execute(array($_SESSION['id']));
         header("Location: index.php");
 ?>
 

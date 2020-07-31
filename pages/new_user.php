52359f3cdd5d0c5abfb59457c20eac4507223939
Buchos <pluyart@gmail.com>
2020-07-31 14:30:17 +0200

    end

--- pages/new_user.php
+++ pages/new_user.php
@@ -22,6 +22,37 @@ if (isset($_POST['forminscription'])) {
                         if ($password == $password2) {
                             $insertmbr = $bdd->prepare("INSERT INTO users(username, password, mail) VALUES(?, ?, ?)");
                             $insertmbr->execute(array($username, $password, $mail));
+
+
+                            
+                            // require 'vendor/autoload.php'; // If you're using Composer (recommended)
+                            // Comment out the above line if not using Composer
+                            require("../sendgrid-php/sendgrid-php.php");
+                            // If not using Composer, uncomment the above line and
+                            // download sendgrid-php.zip from the latest release here,
+                            // replacing <PATH TO> with the path to the sendgrid-php.php file,
+                            // which is included in the download:
+                            // https://github.com/sendgrid/sendgrid-php/releases
+                            
+                            $email = new \SendGrid\Mail\Mail();
+                            $email->setFrom("aptgbc@gmail.com", "Example User");
+                            $email->setSubject("Sending with SendGrid is Fun");
+                            $email->addTo("pluyart@gmail.com", "Example User");
+                            $email->addContent("text/plain", "and easy to do anywhere, even with PHP");
+                            $email->addContent(
+                                "text/html",
+                                "<strong>and easy to do anywhere, even with PHP</strong>"
+                            );
+                            $sendgrid = new \SendGrid(getenv('SENDGRID_API_KEY'));
+                            try {
+                                $response = $sendgrid->send($email);
+                                print $response->statusCode() . "\n";
+                                print_r($response->headers());
+                                print $response->body() . "\n";
+                            } catch (Exception $e) {
+                                echo 'Caught exception: '. $e->getMessage() ."\n";
+                            }
+
                             $erreur = "Votre compte a bien été créé ! <a href=\"login.php\">Me connecter</a>";
                         } else {
                             $erreur = "Vos mots de passes ne correspondent pas !";
